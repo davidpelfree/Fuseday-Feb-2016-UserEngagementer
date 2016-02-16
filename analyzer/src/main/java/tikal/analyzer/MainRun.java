74b972fab81b545de527aebf180563298edc091c
Guy <guyt@feedvisor.com>
2016-02-16 11:18:37 +0200

    first phase

---
+++ analyzer/src/main/java/tikal/analyzer/MainRun.java
@@ -0,0 +1,16 @@
+package tikal.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainRun 
+{
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainRun.class, args);
+    }
+
+}
+
